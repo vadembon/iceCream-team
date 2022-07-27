7c4c27bcdc8f8071e1127abf4be16214a5ee1ce0
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-07-28 01:21:35 +0300

    добавил новые скрипты

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,16 +1,40 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open-btn'),
+//     closeMenuBtn: document.querySelector('.menu-close-btn'),
+//     menu: document.querySelector('.burger-menu'),
+//     body: document.querySelector('body'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
+
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open-btn'),
+    openMenuBtn: document.querySelector('.menu-open__btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.burger-menu'),
     body: document.querySelector('body'),
+    closeMenuLink: document.querySelector('[data-nav-link]'),
+    closeMenuLinkContact: document.querySelector('[data-nav-linkC]'),
+    closeMenuLinkProduct: document.querySelector('[data-nav-linkP]'),
+    closeMenuLinkProduct: document.querySelector('[data-nav-linkB]'),
+    closeMenuLinkProduct: document.querySelector('[data-nav-linkD]'),
   };
-
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
+  refs.closeMenuLink.addEventListener('click', toggleMenu);
+  refs.closeMenuLinkContact.addEventListener('click', toggleMenu);
+  refs.closeMenuLinkProduct.addEventListener('click', toggleMenu);
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-none');
+    refs.menu.classList.toggle('menu-scroll');
     refs.body.classList.toggle('no-scroll');
   }
 })();
