d628c2753384a4d5fb437350098f567af3e41bc9
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-09-11 18:28:25 +0300

    Update scroll-header.js
    fixed blur

--- src/js/scroll-header.js
+++ src/js/scroll-header.js
@@ -4,8 +4,10 @@ window.onscroll = function () {
     // Если прокрутка есть, то делаем блок прозрачным
     document.querySelector('.header-scroll').style.background =
       'rgba(255, 184, 202, 0.7)';
+    document.querySelector('.header').style.backdropFilter = 'blur(6px)';
   } else {
     // Если нет, то делаем его полностью видимым
     document.querySelector('.header-scroll').style.background = 'transparent';
+    document.querySelector('.header').style.backdropFilter = 'none';
   }
 };
